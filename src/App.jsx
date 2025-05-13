82e4581efa0b8ad38358d902c6c0868b16ad67fb
Samuel <ajiboyeadebayo901@gmail.com>
2025-05-13 12:20:20 +0100

    progress 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,37 @@ import Nav from './components/Nav'
 
 const App = () => {
   return (
-    <div className='w-full px-40'>
+    <div className='w-full px-20'>
       {/* <Nav/> */}
       <section id='about-me' className='flex items-center w-full py-10'>
-        <div className='w-[50%] h-[500px] flex justify-center items-center'>
+        <div className='w-[40%] h-[500px] flex justify-center items-center'>
           <div className='w-[300px] h-[300px] rounded-full overflow-hidden'>
-            <img className='w-full' src="/dp.jpeg" alt="profile image" />
+            <img className='w-full hiddenpy-10' src="/dp.jpeg" alt="profile image" />
           </div>
         </div>
-        <div className="w-[50%]">
-          <h3>A Fullstack Web Developer</h3>
-          <h2>Ajiboye Samuel</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores facilis qui reprehenderit cum nobis voluptates quidem cupiditate praesentium mollitia? Architecto a placeat assumenda, commodi blanditiis esse mollitia corporis. Nesciunt!</p>
-          <a href=""><button className='p-5'>Contact</button></a> 
+        <div className="w-[60%]">
+          <h3 className=' my-3 '>A Fullstack Web Developer</h3>
+          <h2 className=' my-3 text-5xl text-white'>Ajiboye Samuel</h2>
+          <p className=' my-3 '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores facilis qui reprehenderit cum nobis voluptates quidem cupiditate praesentium mollitia? Architecto a placeat assumenda, commodi blanditiis esse mollitia corporis. Nesciunt!</p>
+          <a href=""><button className='hover:bg-blue-700 rounded-4xl py-2 px-3 border-3 border-white transition 1s'>Contact</button></a> 
         </div>
       </section>
 
-      <section id='Skills' className='flex'>
-        <h2>Skills</h2>
-        <div className='bg-blue-300 p-5'>HTML</div>
+      <section id='Skills' className='py-10'>
+        <h2 className='mb-3 text-3xl'>Skills</h2>
+        <ul className='flex gap-2'>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+          <li className="bg-blue-700 rounded-2xl py-2 px-3 flex">HTML</li>
+        </ul>
       </section>
 
-      <section id='Hobbies'>
-      <h2>Hobbies</h2>
+      <section id='Hobbies' className='py-10'>
+      <h2 className='mb-3 text-3xl'>Hobbies</h2>
         <ul>
           <li>Gaming</li>
           <li>Football</li>
@@ -33,20 +41,15 @@ const App = () => {
         </ul>
       </section>
 
-      <section id='passions'>
+      <section id='passions' className='py-10'>
       <h2>Passion</h2>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab fuga nostrum eos facere quaerat distinctio molestias ut veritatis mollitia neque alias consequatur voluptatum, doloremque perferendis ipsam in pariatur voluptatem?
       </section>
 
-      <section id='academic-background'>
+      <section id='academic-background' className='py-10'>
         <h2>Academic Background</h2>
         <p></p>
       </section>
-
-      <section id='work-experience'></section>
-
-      <section id='certification'></section>
-
     </div>
     
   )
