06ca334b856a58672e4966e393a02780cac07f17
Samuel <ajiboyeadebayo901@gmail.com>
2025-05-13 11:01:38 +0100

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import Nav from './components/Nav'
 
 const App = () => {
   return (
-    <div className='text-center text-9xl'>App</div>
+    <div>
+      {/* <Nav/> */}
+      <section id='about-me' className='flex items-center'>
+        <div className='w-[50%}'>
+          <div>
+            <img src="" alt="profile image" />
+          </div>
+        </div>
+        <div className="w-[50%}'">
+          <h3>A Fullstack Web Developer</h3>
+          <h2>Ajiboye Samuel</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis maiores facilis qui reprehenderit cum nobis voluptates quidem cupiditate praesentium mollitia? Architecto a placeat assumenda, commodi blanditiis esse mollitia corporis. Nesciunt!</p>
+          <a href=""><button className='p-5'>Contact</button></a> 
+        </div>
+      </section>
+
+      <section id='Skills' className='flex'>
+        <h2>Skills</h2>
+        <div className='bg-blue-300 p-5'>HTML</div>
+      </section>
+
+      <section id='Hobbies'>
+      <h2>Hobbies</h2>
+        <ul>
+          <li>Gaming</li>
+          <li>Football</li>
+          <li>Browsing lit designs</li>
+        </ul>
+      </section>
+
+      <section id='passions'>
+      <h2>Passion</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui ab fuga nostrum eos facere quaerat distinctio molestias ut veritatis mollitia neque alias consequatur voluptatum, doloremque perferendis ipsam in pariatur voluptatem?
+      </section>
+
+      <section id='academic-background'>
+        <h2>Academic Background</h2>
+        <p></p>
+      </section>
+
+      <section id='work-experience'></section>
+
+      <section id='certification'></section>
+
+    </div>
+    
   )
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
-import {headerLogo} from '../assets/images'
-import { navLinks } from '../constants'
-import { hamburger } from '../assets/icons'
 const Nav = () => {
   return (
     <header className='padding-x py-8 absolute z-10 w-full'>
       <nav className='flex justify-between items-center max-container'>
         <a href="/">
-           <img src={headerLogo} alt="Logo" className='w-[130px] h-[29px]'/>
+           {/* <img src={headerLogo} alt="Logo" className='w-[130px] h-[29px]'/> */}
         </a>
         <ul className='flex-1 flex justify-end items-center gap-16 max-lg:hidden bg-transparent self-end mr-1'>
-           {navLinks.map((items)=>{
-            return(
-              <li key={items.label}>
-                <a href={items.href} className='font-montserrat leading-normal text-lg text-slate-gray hover:text-[#FF6452] duration-200'>{items.label}</a>
+              <li>
+                <a className='font-montserrat leading-normal text-lg text-slate-gray hover:text-[#FF6452] duration-200'>Home</a>
               </li>
-            )
-           })}
         </ul>
         <div className='hidden max-lg:block'>
-          <img src={hamburger} alt="Hamburger" className='w-[30px] h-[30px]'/>
+          <img src="" alt="Hamburger" className='w-[30px] h-[30px]'/>
         </div>
       </nav>
     </header>
